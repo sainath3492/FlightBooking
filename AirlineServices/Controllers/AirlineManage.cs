0f329603e2724801b39c9f28efd2fbe3545ee187
cogdotnet1094 <cogdotnet1094@CTSDOTNET388>
2022-07-13 13:11:34 +0530

    manage airline servcie added

---
+++ AirlineServices/Controllers/AirlineManage.cs
@@ -0,0 +1,43 @@
+﻿using AirlineServices.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirlineServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AirlineManage : ControllerBase
+    {
+        DatabaseContext _context;
+
+        public AirlineManage(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("/Airline_Master")]
+
+        public IEnumerable<Airline> GetAirline_Master()
+        {
+            var data = _context.Airline_Master.ToList();
+            return data;
+
+        }
+
+        [HttpPost]
+        [Route("/SaveAirline")]
+        public int SaveAirline(Airline flight_Bookings)
+        {
+            _context.Airline_Master.Add(flight_Bookings);
+            _context.SaveChanges();
+            var data = _context.Airline_Master.ToList().LastOrDefault();
+            return data.AirlineID;
+
+        }
+    }
+}
---
+++ AirlineServices/Database/Airline.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirlineServices.Database
+{
+    public class Airline
+    {
+        [Key]
+        public int AirlineID { get; set; }
+
+        public string AirlineName { get; set; }
+
+        public string From_Place { get; set; }
+
+        public string To_Place { get; set; }
+
+
+        public DateTime Start_Date { get; set; }
+
+        public DateTime End_Date { get; set; }
+
+        public string Scheduled_Days { get; set; }
+
+        public string Instrument { get; set; }
+
+        public int Business_Seats { get; set; }
+
+        public int Non_Business_Seats { get; set; }
+
+        public string Ticket_Cost { get; set; }
+
+        public string Rows { get; set; }
+
+        public string Meal { get; set; }
+    }
+}
